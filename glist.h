600252fe28fc7f52ae907785b50beec171c70279
Lucas Bachur <lucasbachur11@gmail.com>
2020-04-30 18:41:21 -0300

    Agregado comentado

--- glist.h
+++ glist.h
@@ -4,10 +4,19 @@
 #define forrapido(lista, iterador) for (GNodo *iterador = lista.inicio; iterador != NULL; iterador = iterador->sig)
 
 
+// La estructura GNodo representa un nodo de una lista enlazada.
+// Su primer elemento es un puntero al dato que esta almacenando y
+// el segundo es un puntero al nodo que le sigue en la lista.
+
 typedef struct _GNodo {
   void *dato;
   struct _GNodo *sig;
 } GNodo;
+
+// La estructura GList representa una lista enlazada.
+// Su primer elemento es un puntero al primer nodo de la lista y
+// el segundo es un puntero al ultimo nodo de la lista.
+
 typedef struct _GList {
   GNodo *inicio;
   GNodo *final;
@@ -19,17 +28,19 @@ typedef int (*Compara) (void *dato1, void *dato2);
 typedef void (*ImprimeArchivo) (void *dato, FILE *archivo);
 typedef GList (*Ordenamiento) (GList lista, Compara comparacion);
 
-// Crea una glist.
+// glist_crear devuelve una nueva estructura del tipo GList vacia.
+// glist_crear: -> GList
 GList glist_crear ();
 
-// Dado un puntero a GList y un dato.
-// Crea un nuevo nodo cuyo puntero, apunta al primer nodo de la lista.
-// Luego se iguala el comienzo al nuevo nodo
+// Toma un puntero a GList y un dato.
+// Crea un nuevo nodo que contiene el dato argumento y lo agrega
+// al comienzo de la lista.
+// glist_agregar_inicio: GList* void* ->
 void glist_agregar_inicio (GList *lista, void* dato);
 
-// Dado un puntero a GList, y un dato.
-// Crea un nuevo nodo cuyo puntero apunta a NULL, y es el siguiente
-// al anterior puntero de la lista.
+// Toma un puntero a GList y un dato.
+// Crea un nuevo nodo que contiene el dato argumento y lo agrega
+// al final de la lista.
 void glist_agregar_final (GList *lista, void* dato);
 
 void glist_imprimir_archivo (GList *lista, ImprimeArchivo funcion, char *nombreArchivoSalida);
